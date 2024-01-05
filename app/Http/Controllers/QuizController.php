549c5f1d1cd66431372e7f585544bc2e12d987c3
ADAMxSTUDIO <adamatstudio@gmail.com>
2024-01-05 10:43:34 +0100

    extend Preventmiddleware

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Quiz;
 use App\Models\Answer;
 use App\Models\Record;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 class QuizController extends Controller
@@ -41,10 +42,11 @@ class QuizController extends Controller
         }
 
         $score = round($correctAnswerCounter * 100 / $totalAnswers);
-        
+        $user_id = Auth::id();
+
         // Insert the record into the database
         // $record = new Record;
-        // $record->user_id = Auth::id();
+        // $record->user_id = ;
         // $record->quiz_id = 1;
         // $record->score = $score;
         // $record->save();
@@ -52,6 +54,10 @@ class QuizController extends Controller
         // Record::find(1)->update([
         //     'score' => $score
         // ]);
+
+        User::find($user_id)->update([
+            'has_passed' => 1
+        ]);
 
         // Redirect to the view where the user views their score then logout
         return view('quiz.completion', compact('score'));
